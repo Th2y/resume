1b55ab79235c94b898c8aec65fc77497c3a51af2
thay.santos-at-538590928221 <thayanecarvalho3@gmail.com>
2025-10-28 03:03:07 -0300

    Vercel

--- src/app/services/http/resume.service.ts
+++ src/app/services/http/resume.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Observable, shareReplay } from 'rxjs';
 import { Resume } from '../../interfaces/resume';
 
@@ -8,15 +9,30 @@ import { Resume } from '../../interfaces/resume';
 })
 export class ResumeService {
   private readonly resumeUrl = '/assets/resume.json';
-
-  constructor(private http: HttpClient) {}
-
   private cache$?: Observable<Resume>;
 
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
   getResume(): Observable<Resume> {
+    if (!isPlatformBrowser(this.platformId)) {
+      // SSR/prerender: não tenta buscar o JSON
+      return of({} as Resume);
+    }
+
     if (!this.cache$) {
       this.cache$ = this.http.get<Resume>(this.resumeUrl).pipe(shareReplay(1));
     }
     return this.cache$;
   }
 }
+function Inject(PLATFORM_ID: any): (target: typeof ResumeService, propertyKey: undefined, parameterIndex: 1) => void {
+  throw new Error('Function not implemented.');
+}
+
+function of(arg0: Resume): Observable<Resume> {
+  throw new Error('Function not implemented.');
+}
+
